8945cc34d79e806153dccdc5c8f7512e7076d7ad
tim <tdixon51793@gmail.com>
2011-12-11 14:34:28 -0600

    Import the default and user's interpreters in interpreters.py, not build

--- source/cytoplasm/build.py
+++ source/cytoplasm/build.py
@@ -1,19 +1,21 @@
-import os, shutil, imp, errors
-from configuration import *
+import os, shutil, errors, interpreters
+from configuration import build_dir
 
 def copy_over():
     "Copy the files not beginning with '_' to the site directory"
+    interpreter_dictionary = interpreters.ImportInterpreters()
     # All of the files that don't begin with '_':
     to_copy = [file for file in os.listdir(".") if not file.startswith("_")]
     for file in to_copy: 
         handled = False
-        for ending in interpreters.keys():
+        for ending in interpreter_dictionary.keys():
             # if the file has a suffix that matches any of the interpreters,
             # parse that file with that interpreter
             if file.endswith(".%s" %(ending)): 
                 # give the interpreter two variables -- the original place and hte destination
-                # of the itnerpreted file.
-                interpreters[ending](file, "%s/%s" %(build_dir, file.replace(".%s" %(ending), "")))
+                # of the interpreted file.
+                destination = "%s/%s" %(build_dir, file.replace(".%s" %(ending), ""))
+                interpreter_dictionary[ending](file, destination)
                 handled = True
                 break
         if not handled:
@@ -21,16 +23,6 @@ def copy_over():
             shutil.copy2(file, build_dir)
 
 def build():
-    # Import the user's _interpreters.py, if it exists in this directory
-    if os.path.exists("_interpreters.py"): 
-        interpreters_module = imp.load_source("_interpreters", "_interpreters.py")
-        try:
-            from _interpreters import interpreters as interpreters_imported
-        except ImportError:
-            # raise this error if _interpreters.py doesn't have a variable called "interpreters"
-            raise errors.InterpreterError("Your _interpreters.py doesn't have anything I can use.")
-        # overwrite the default interpreters dictionary with the user's
-        interpreters.update(interpreters_imported)
     # Create the build directory, if it doesn't exist
     if not os.path.exists(build_dir):
         os.mkdir(build_dir)
--- source/cytoplasm/interpreters.py
+++ source/cytoplasm/interpreters.py
@@ -1,3 +1,17 @@
+def ImportInterpreters():
+    import os, imp
+    from configuration import interpreters
+    if os.path.exists("_interpreters.py"):
+        interpreters_module = imp.load_source("_interpreters", "_interpreters.py")
+        try:
+            from _interpreters import interpreters as interpreters_imported
+        except ImportError:
+            # raise this error if _interpreters.py doesn't have a variable called "interpreters"
+            raise errors.InterpreterError("Your _interpreters.py doesn't have anything I can use.")
+        # overwrite the default interpreters dictionary with the user's
+        interpreters.update(interpreters_imported)
+    return interpreters
+
 def SaveReturned(fn):
     '''Some potential interpreters, like Mako, don't give you an easy way to save to a destination.
     In these cases, simply use this function as a decorater.'''
@@ -7,3 +21,4 @@ def SaveReturned(fn):
         f.write(fn(file, **kwargs))
         f.close()
     return InterpreterWithSave
+
