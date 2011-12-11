8c58e340f0cf9b815d2033cd0a1eb401ea44232b
tim <tdixon51793@gmail.com>
2011-12-11 14:01:20 -0600

    Wrote a mok interpreter.

--- source/cytoplasm/configuration.py
+++ source/cytoplasm/configuration.py
@@ -3,7 +3,24 @@
 # Specify the build directory, where the built site will be copied to
 build_dir = "_build"
 
+from interpreters import SaveReturned
 # A dictionary where the key of a suffix is the function that should handle that file
 # The function is given two arguments, the filename and where that file should be put 
 # after it's finished
 interpreters = {}
+
+# An interpreter for mako, which will be prett essential both for "static" files and for controller
+import mako.lookup, mako.template
+# Mako should look for templates to include in the current directory.
+# This should let you include things like "_templates/site.mako"
+mako_lookup = mako.lookup.TemplateLookup(directories=['.'])
+# Mako doesn't come with an easy built-in way to save the output to a certain file;
+# this decorator does that.
+@SaveReturned
+def mako_interpreter(file, **kwargs):
+    # pass kwargs to the mako template
+    page = mako.template.Template(filename=file, lookup=mako_lookup)
+    return page.render_unicode(**kwargs)
+
+interpreters["mako"] = mako_interpreter
+
