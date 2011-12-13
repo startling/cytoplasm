a74209c06577bb2815c623b688e6f56532412a4f
tim <tdixon51793@gmail.com>
2011-12-13 17:25:55 -0600

    SaveReturned method in interpreters.py can deal with file-like objects.

--- source/cytoplasm/interpreters.py
+++ source/cytoplasm/interpreters.py
@@ -1,10 +1,15 @@
 def SaveReturned(fn):
     '''Some potential interpreters, like Mako, don't give you an easy way to save to a destination.
     In these cases, simply use this function as a decorater.'''
-    def InterpreterWithSave(file, destination, **kwargs):
-        f = open(destination, "w")
+    def InterpreterWithSave(source, destination, **kwargs):
+        # under some circumstances, this should be able to write to file-like objects;
+        # so if destination is a string, assume it's a path; otherwise, it's a file-like object
+        if isinstance(destination, str):
+            f = open(destination, "w")
+        else:
+            f = destination
         # pass **kwargs to this function.
-        f.write(fn(file, **kwargs))
+        f.write(fn(source, **kwargs))
         f.close()
     return InterpreterWithSave
 
