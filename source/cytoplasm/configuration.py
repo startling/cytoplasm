426d3fd0572d0b648ceb7d5394b555f4a7c65a1e
tim <tdixon51793@gmail.com>
2011-12-14 12:36:31 -0600

    Raise an error if the user doesn't have a _config.py
    This raises a less cryptic error.

--- source/cytoplasm/configuration.py
+++ source/cytoplasm/configuration.py
@@ -1,6 +1,7 @@
 # This module contains the user's configurations, to be accessed like:
 # `print cytoplasm.configuration.build_dir`
 import os, imp
+from .errors import CytoplasmError
 
 # If the user has a file called _config.py, import that.
 # The user's _config.py should "from cytoplasm.defaults import *" if they want to use
@@ -8,3 +9,5 @@ import os, imp
 if os.path.exists("_config.py"):
     imp.load_source("_config", "_config.py")
     from _config import *
+else:
+    raise CytoplasmError("You don't seem to have a configuration file at '_config.py'.")
