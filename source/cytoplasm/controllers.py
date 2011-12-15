255db8f5c19015b8a3a8b36e35e3534424f46196
tim <tdixon51793@gmail.com>
2011-12-15 15:42:42 -0600

    All controllers should take a templates directory.

--- source/cytoplasm/controllers.py
+++ source/cytoplasm/controllers.py
@@ -11,12 +11,13 @@ def controllerclass(name):
 
 class Controller(object):
     "Controllers take data from some files, do stuff with it, and write it to the build directory."
-    def __init__(self, data, build):
-        # where this controller finds its data
+    def __init__(self, data, destination, templates="_templates"):
+        # take three arguments: the source directory, the destination directory, and, optionally,
+        # a directory wherein the templates reside.
         self.data_directory = data
-        # where it should write files to...
-        self.destination_directory = build
-        # create the destination directort, if it doesn't exist
+        self.destination_directory = destination
+        self.templates_directory = templates
+        # create the destination directory, if it doesn't exist
         if not os.path.exists(self.destination_directory): os.mkdir(self.destination_directory)
     
     def __call__(self):
