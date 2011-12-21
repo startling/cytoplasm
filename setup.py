492e75ab2192e8ba3b3d792b82bb66f2fd3e04ad
tim <tdixon51793@gmail.com>
2011-12-21 02:04:10 -0600

    Added a bunch of fields to setup.py in order to register it with PyPI.

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name = "cytoplasm",
+    name = "Cytoplasm",
     version = "0.01",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    url = "http://cytoplasm.somethingsido.com",
+    keywords = ["blogging", "site compiler", "blog compiler"],
     description = "A static, blog-aware website generator written in python.",
     packages = ['cytoplasm'],
     scripts = ['scripts/cytoplasm'],
-    install_requires=['mako']
+    requires = ['Mako'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+        ]
 )
         
