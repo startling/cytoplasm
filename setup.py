a3132865cdab8f4754b5d4fbda392df729a1745c
tim <tdixon51793@gmail.com>
2011-12-21 01:48:13 -0600

    Changed the directory structure some.
    The package is now in a directory named just `cytoplasm`. The script is
    in a new directory named `script.`

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name = "cytoplasm",
     version = "0.01",
     description = "A static, blog-aware website generator written in python.",
-    package_dir = {'': 'source'},
     packages = ['cytoplasm'],
-    scripts = ['cytoplasm'],
+    scripts = ['scripts/cytoplasm'],
     install_requires=['mako']
 )
         
