1b117d99d11b05b76a95075bb449bbf7b7f06778
tim <tdixon51793@gmail.com>
2011-12-26 21:46:50 -0600

    You can now optionally give get_config a directory argument.
    It'll still fall back on the global source_dir, though.

--- cytoplasm/configuration.py
+++ cytoplasm/configuration.py
@@ -8,11 +8,13 @@ from cytoplasm.errors import CytoplasmError
 # This a global variable to be shared and possibly changed across all of the cytoplasm modules.
 source_dir = "."
 
-def get_config():
+def get_config(dir=None):
+    # if there isn't a specified directory in the arguments, use the global source_dir.
+    if dir == None: dir = source_dir
     # If the user has a file called _config.py, import that and return the module.
     # The user's _config.py should "from cytoplasm.defaults import *" if they want to use
     # some of the defaults.
-    config_file = os.path.join(source_dir, "_config.py")
+    config_file = os.path.join(dir, "_config.py")
     if os.path.exists(config_file):
         imp.load_source("_config", config_file)
         import _config
