dd3f9da264b0a1d62c8d3ec794ff825ac18428e1
tim <tdixon51793@gmail.com>
2012-02-23 04:32:40 -0600

    Most of the interpreter methods take `site` arguments now.
    This way they see the site's configuration, not that silly global
    function thing we had before.

--- cytoplasm/interpreters.py
+++ cytoplasm/interpreters.py
@@ -7,13 +7,13 @@ from cytoplasm import configuration
 from cytoplasm.errors import InterpreterError
 
 
-def interpreted_filename(file):
+def interpreted_filename(file, site):
     "Intelligently return the interpreted filename of a file."
     # get the last suffix:
     suffix = file.split(".")[-1]
     # if the suffix is in interpreters.keys() the destination is everything
     # but that last suffix.
-    if suffix in configuration.get_config().interpreters.keys():
+    if suffix in site.config.interpreters.keys():
         return ".".join(file.split(".")[:-1])
     # otherwise, it's the whole thing.
     else:
@@ -38,17 +38,18 @@ def default_interpreter(source, destination, **kwargs):
     # destination and source are always file objects, so you can copyfileobj.
     shutil.copyfileobj(source, destination)
 
-def interpret(source, destination, **kwargs):
+
+def interpret(source, destination, site, **kwargs):
     """Interpret a file to a destination with an interpreter according to
     its suffix.
     """
     # open the destination and write to it.
     with open(destination, "w") as destination_file:
         # and, since we now have a file-like object, use interpret_to_filelike
-        interpret_to_filelike(source, destination_file, **kwargs)
+        interpret_to_filelike(source, destination_file, site, **kwargs)
 
 
-def interpret_to_filelike(source, destination, **kwargs):
+def interpret_to_filelike(source, destination, site, **kwargs):
     """Interpret a file to a file-like object with an interpreter according
     to its suffix."""
     # figure out the last suffix of the file, to use to determine which
@@ -56,14 +57,14 @@ def interpret_to_filelike(source, destination, **kwargs):
     ending = source.split(".")[-1]
     # and then interpret, based on the ending of the file!
     with open(source, "r") as f:
-        interpret_filelike(f, destination, ending, **kwargs)
+        interpret_filelike(f, destination, site, ending, **kwargs)
 
 
-def interpret_filelike(source, destination, suffix, **kwargs):
+def interpret_filelike(source, destination, site, suffix, **kwargs):
     """Given some two file-like objects and a suffix, interpret the source
     according to the given suffix and write it to the destination file object.
     """
     # get the list of interpreters from the configuration
-    interpreters = configuration.get_config().interpreters
+    interpreters = site.config.interpreters
     # and then interpret, based on the ending of the file!
     interpreters.get(suffix, default_interpreter)(source, destination, **kwargs)
