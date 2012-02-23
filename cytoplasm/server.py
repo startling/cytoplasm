725d0015f8638014914fbc3eac5c02ea919b1f0d
tim <tdixon51793@gmail.com>
2012-02-23 05:20:08 -0600

    Fixed the server for the new way of doing things.
    It still uses global variables though. hrm.

--- cytoplasm/server.py
+++ cytoplasm/server.py
@@ -6,6 +6,7 @@ import os
 import sys
 import cytoplasm
 
+
 # make this work in either Python 2.x or 3.x
 if sys.version_info.major >= 3:
     from http.server import SimpleHTTPRequestHandler, HTTPServer
@@ -13,8 +14,11 @@ else:
     from SimpleHTTPServer import SimpleHTTPRequestHandler
     from BaseHTTPServer import HTTPServer
 
+
 # keep track of when things were last built in this global variable
 most_recent_time = 0
+# keep track of the site in this global variable
+site = None
 
 
 def serve(port, rebuild, event=None):
@@ -22,11 +26,14 @@ def serve(port, rebuild, event=None):
     # keep track of the most recently modified time in global variable
     # most_recent_time
     global most_recent_time
-    most_recent_time = most_recent(".")
-    # rebuild the site first.
-    cytoplasm.build()
+    global site
+    # create a site and rebuild it first.
+    site = cytoplasm.Site(".")
+    site.build()
+    # set the most recent time.
+    most_recent_time = most_recent()
     # change to the build directory, where things are to be served from.
-    os.chdir(cytoplasm.configuration.get_config().build_dir)
+    os.chdir(site.config.build_dir)
     # use either SimpleHTTPRequestHandler or RebuildHandler, depending on
     # whether rebuild is True.
     if rebuild:
@@ -39,11 +46,12 @@ def serve(port, rebuild, event=None):
     httpd.serve_forever()
 
 
-def most_recent(directory):
+def most_recent():
     """Determine the most recent modified time in the source directory,
     ignoring dotfiles and _build.
     """
-    build_dir = cytoplasm.configuration.get_config().build_dir
+    directory = site.source
+    build_dir = site.config.build_dir
     # get the candidate files:
     files = [f for f in os.listdir(directory) if f != build_dir and not
             f.startswith(".")]
@@ -60,7 +68,7 @@ class RebuildHandler(SimpleHTTPRequestHandler):
         # declare most_recent_time global; we'll be changing it later.
         global most_recent_time
         # figure out the most recent time edited in the source directory
-        new_recent = most_recent("..")
+        new_recent = most_recent()
         # only build the site if the new most recent is more recent than the
         # old one, i.e. if one or more of the files has been edited.
         if new_recent > most_recent_time:
@@ -68,6 +76,6 @@ class RebuildHandler(SimpleHTTPRequestHandler):
             most_recent_time = new_recent
             # Build the site from the source directory
             print("Rebuilding your Cytoplasm site...")
-            cytoplasm.build("..")
+            site.build()
         # Call SimpleHTTPRequestHandler.handle(), so it can do stuff there too.
         SimpleHTTPRequestHandler.handle(self)
